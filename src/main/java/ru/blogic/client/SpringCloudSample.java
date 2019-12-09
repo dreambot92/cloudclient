736cbbb820fc809d4ed9ceb78be7d325cb8ef6de
Шабалин <ishabalin@it.ru>
2019-12-09 09:10:37 +0500

    cloud client

--- src/main/java/ru/blogic/client/SpringCloudSample.java
+++ src/main/java/ru/blogic/client/SpringCloudSample.java
@@ -3,6 +3,7 @@ package ru.blogic.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringCloudSample {
 
